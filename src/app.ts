41150df03f185a37a269970027a011dae36e843f
Dhairya915 <dhairyaghardesia9@gmail.com>
2026-07-29 11:51:33 +0530

    setup global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,20 @@
-import express from 'express';
+import express, { type NextFunction, type Request, type Response } from 'express';
 import healthRoute from './routes/health.route';
+import type { HttpError } from './utils/HttpError';
+import { logger } from './lib/logger';
 
 const app = express();
 
 app.use(express.json());
 app.use('/api/v1', healthRoute);
+
+app.use((err:HttpError , req:Request , res:Response , next:NextFunction) => {
+    logger.error(err.message);
+    const statusCode = err.statusCode || 500;
+
+    res.status(statusCode).json({
+        errors: [{ type: err.name, msg: err.message, path: '', location: '' }],
+    })
+})
 
 export default app;
--- src/controllers/health.controller.ts
+++ src/controllers/health.controller.ts
@@ -1,4 +1,5 @@
 import type { Request, Response } from 'express';
+import { HttpError } from '../utils/HttpError';
 
 export function getHealth(req: Request, res: Response) {
   res.status(200).json({ status: 'ok' });
